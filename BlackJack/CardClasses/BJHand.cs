222579ff6ab3b5e1563df5a62d02d3f6b5ab2fc4
AlexBedney <bedneya@my.lanecc.edu>
2022-05-27 13:26:39 -0700

    Added code to frist problem complertely and partially to problem 2

---
+++ BlackJack/CardClasses/BJHand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardClasses
+{
+    public class BJHand : Hand
+    {
+        public BJHand() : base() { }
+        
+        public BJHand(Deck d, int numCards) : base(d, numCards) { }
+
+        public bool HasAce
+        {
+            get
+            {
+                return HasAce;
+            }
+        }
+
+        public int Score
+        {
+            get
+            {
+                int score = 0;
+                foreach(Card c in cards)
+                {
+                    if (c.IsFaceCard())
+                        score += 10;
+                    else
+                        score += c.Value;
+                }
+
+                if (this.HasAce && score <= 11)
+                    score += 10;
+
+                return score;
+            }
+        }
+
+        public bool IsBusted
+        {
+            get
+            {
+                if (Score > 21)
+                    return true;
+                else
+                    return false;
+            }
+        }
+
+
+    }
+}
---
+++ BlackJack/CardClasses/Hand.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardClasses
+{
+    public class Hand
+    {
+        protected List<Card> cards = new List<Card>();
+
+        public Hand() { }
+
+        public Hand(Deck d, int numCard)
+        {
+            for (int i = 0; i < numCard; i++)
+                cards.Add(d[i]);
+        }
+
+        public int NumCards
+        {
+            get
+            {
+                return cards.Count;
+            }
+        }
+
+        public void AddCard(Card c)
+        {
+            cards.Add(c);
+        }
+
+        
+
+        public Card GetCard(int index)
+        {
+            return cards[index];
+        }
+
+        public bool HasCard(Card c)
+        {
+            foreach (Card card in cards)
+                if (card == c)
+                    return true;
+            return false;
+        }
+
+        public bool HasCard(int value)
+        {
+            foreach (Card card in cards)
+                if (card.Value == value)
+                    return true;
+            return false;
+        }
+
+        public bool HasCard(int value, int suit)
+        {
+            foreach (Card card in cards)
+                if (card.Value == value && card.Suit == suit)
+                    return true;
+            return false;
+        }
+
+        // Looked at in break room during class, exempted due to strange bug
+        /*
+        public Card DiscardCard(int index)
+        {
+            Card out = cards[index];
+            return out;
+        }
+        */
+
+        public int IndexOf(Card c)
+        {
+            for (int i = 0; i < cards.Count; i++)
+                if (cards[i] == c)
+                    return i;
+            return 0;
+        }
+
+        public int IndexOf(int value)
+        {
+            for (int i = 0; i < cards.Count; i++)
+                if (cards[i].Value == value)
+                    return i;
+            return 0;
+        }
+
+        public int IndexOf(int value, int suit)
+        {
+            for (int i = 0; i < cards.Count; i++)
+                if (cards[i].Value == value && cards[i].Suit == suit)
+                    return i;
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            string output = "";
+            foreach (Card c in cards)
+                output += c.ToString() + "\n";
+            return output;
+        }
+    }
+}
